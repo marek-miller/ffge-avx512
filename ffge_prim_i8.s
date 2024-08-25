5dc79ee12ed0e3b014eb4eea69a6a69690c4ebe1
⧉⧉⧉ <marek.miller@protonmail.com>
2024-08-25 20:45:05 +0200

    Add comments to ffge_prim_i8.s

--- ffge_prim_i8.s
+++ ffge_prim_i8.s
@@ -21,17 +21,26 @@ default rel
 
 global ffge_prim_i8
 
+
 section .rodata
-	FFGE_PRIM	dq 0x7fffffff
+	FFGE_PRIM	dq 0x7fffffff		; 2^31 - 1, a Mersenne prime
+
 
 section .note.GNU-stack
 section .text
 	extern ffge_pivot_find_i8
 
+
 ;
 ; int64_t ffge_prim_i8(int64_t *m, size_t n)
 ;
 ffge_prim_i8:
+	; if ( n == 0 ) return 0;
+	xor		rax, rax
+	test		rsi, rsi
+	jz		.rt
+
+	; store registers, align the stack to 0x10
 	push		r15
 	push		r14
 	push		r13
@@ -44,25 +53,29 @@ ffge_prim_i8:
 
 	; initialize state
 	mov		rax, 0xff		; full-rank flags
-	mov		r12, rdi		; current diagonal element
+	mov		r12, rbx		; current diagonal element
 	xor		r14, r14		; size_t pv, pivot column/row
 	mov		r15, r13
 	imul		r15, 64			; size of row in bytes
 	vpbroadcastq	zmm14, [FFGE_PRIM]
 	vpxorq		zmm15, zmm15
 
+	; r14 = pv, pivot columns/rows in the .l0 loop
+	; r12 points to m[pv*n + pv]
 .l0:	mov		rdi, rbx
 	mov		rsi, r13
 	mov		rdx, r14
 	mov		rcx, rax
 	call		ffge_pivot_find_i8
 
-	mov		r8, r14
-	add		r8, 1
-	cmp		r8, r13
+	; rdx = i, matrix rows in the .l1 loop
+	mov		rdx, r14
+	add		rdx, 1
+	cmp		rdx, r13
 	jae		.rt
 
-	mov		r11, r8
+	; r11 points to m[i*n + pv]
+	mov		r11, rdx
 	imul		r11, r13
 	add		r11, r14
 	imul		r11, 64
@@ -71,31 +84,36 @@ ffge_prim_i8:
 	vmovdqa64	zmm0, [r12]
 .l1:	vmovdqa64	zmm1, [r11]
 
+	; rcx = j, matrix columns in the .l2 loop
 	mov		rcx, r14
 	add		rcx, 1
 
-	mov		r9, r8
-	imul		r9, r13
-	add		r9, rcx
-	imul		r9, 64
-	add		r9, rbx
-
+	; r10 points to m[pv*n + j]
 	mov		r10, r14
 	imul		r10, r13
 	add		r10, rcx
 	imul		r10, 64
 	add		r10, rbx
 
+	; r9 points to m[i*n + j]
+	mov		r9, rdx
+	imul		r9, r13
+	add		r9, rcx
+	imul		r9, 64
+	add		r9, rbx
+
 .l2:	vmovdqa64	zmm2, [r10]
 	vmovdqa64	zmm3, [r9]
 
+	; compute zmm3 =
+	;     m[i*n + j] * m[pv*n + pv] - m[i*n + pv] * m[pv*n + j]
 	vpmullq		zmm3, zmm3, zmm0
 	vpmullq		zmm2, zmm2, zmm1
 	vpsubq		zmm3, zmm3, zmm2
 
+	; compute zmm6 = zmm3 % FFGE_PRIM
 	vpmovq2m	k3, zmm3
 	vpabsq		zmm4, zmm3
-
 	vpandq		zmm5, zmm4, zmm14
 	vpsraq		zmm6, zmm4, 31
 	vpandq		zmm7, zmm6, zmm14
@@ -106,13 +124,12 @@ ffge_prim_i8:
 	vpsraq		zmm6, zmm5, 31
 	vpaddq		zmm5, zmm5, zmm6
 	vpandq		zmm5, zmm5, zmm14
-
 	vpsubq		zmm6, zmm5, zmm14
 	vpmovq2m	k6, zmm6
-
 	vpsubq		zmm5 {k3}, zmm15, zmm5
 	vmovdqa64	zmm6, zmm15
 	vmovdqa64	zmm6 {k6}, zmm5
+
 	vmovdqa64	[r9], zmm6
 
 	add		r9, 64
@@ -121,11 +138,12 @@ ffge_prim_i8:
 	cmp		rcx, r13
 	jb		.l2
 
+	; zero the matrix elements "below" current diagonal m[pv*n + pv]
 	vmovdqa64	 [r11], zmm15
 
 	add		r11, r15
-	inc		r8
-	cmp		r8, r13
+	inc		rdx
+	cmp		rdx, r13
 	jb		.l1
 
 	add		r12, r15
@@ -134,11 +152,11 @@ ffge_prim_i8:
 	cmp		r14, r13
 	jb		.l0
 
-.rt:	pop		rbx
+	pop		rbx
 	pop		r12
 	pop		r13
 	pop		r14
 	pop		r15
 
-	vzeroupper
+.rt:	vzeroupper
 	ret
